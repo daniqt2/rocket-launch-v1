a0b7c86b24ed5f0a4618a1b5f37e60a4b8eb908d
Dani Quesada <daniela.dudyfit@gmail.com>
2023-09-22 23:54:54 +0200

    v1 chat bot

---
+++ handlers.py
@@ -0,0 +1,7 @@
+@bot.message_handler(commands=['start', 'hello'])
+def send_welcome(message):
+    bot.reply_to(message, "Howdy, how are you doing?")
+
+@bot.message_handler(func=lambda msg: True)
+def echo_all(message):
+    bot.reply_to(message, message.text)
