3992b0e6434be69bb24d6dfebaa27013c6fa9c9b
Dani Quesada <daniela.dudyfit@gmail.com>
2023-09-24 19:53:17 +0200

    added flask

--- bot.py
+++ bot.py
@@ -1,12 +1,15 @@
 import os
-
+from flask import Flask, request, Response
 
 import telebot
 from dotenv import load_dotenv
 from handlers import *
 
+app = Flask(__name__)
 
-def main():
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
     load_dotenv()
     print(os.environ.get('BOT_TOKEN'))
 
@@ -14,13 +17,17 @@ def main():
 
     if BOT_TOKEN:
         bot = telebot.TeleBot(BOT_TOKEN)
-        
+
         initBot(bot,61695)
-        
+
         bot.infinity_polling()
     else:
         print('Error with BOT INIT')
+    if request.method == 'POST':
 
-
+        return Response('ok', status=200)
+    else:
+        return "<h1>Welcome!</h1>"
+ 
 if __name__ == '__main__':
-    main()
+   app.run(debug=True)
