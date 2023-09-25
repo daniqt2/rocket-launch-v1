b4e564187340eec49e21cf12da2785ebc23ada9f
Dani Quesada <daniela.dudyfit@gmail.com>
2023-09-26 01:40:20 +0200

    flask last

--- Dockerfile
+++
@@ -1,7 +0,0 @@
-FROM python:3.8 AS bot
-COPY requirements.txt /app/
-WORKDIR /app
-COPY . /app
-RUN pip install -r requirements.txt
-EXPOSE 8080
-CMD python3 bot.py --hostname 0.0.0.0 --port $PORT
--- bot.py
+++ bot.py
@@ -3,6 +3,13 @@ import os
 import telebot
 from dotenv import load_dotenv
 from handlers import *
+import flask
+
+app = flask.Flask(__name__)
+
+WEBHOOK_HOST = os.environ.get('WEBHOOK_HOST')
+WEBHOOK_PORT = os.environ.get('PORT') 
+WEBHOOK_LISTEN = '0.0.0.0'  # In some VPS you may need to put here the IP addr
 
 def main():
     load_dotenv()
@@ -11,6 +18,8 @@ def main():
 
     if BOT_TOKEN:
         bot = telebot.TeleBot(BOT_TOKEN)
+
+        bot.remove_webhook()
         
         initBot(bot,61695)
         print("initiated bot")
@@ -18,6 +27,13 @@ def main():
     else:
         print('Error with BOT INIT')
 
-
-if __name__ == '__main__':
+@app.route('/', methods=['GET', 'HEAD'])
+def index():
     main()
+    return ''
+    
+
+# Start flask server
+app.run(host=WEBHOOK_LISTEN,
+        port=WEBHOOK_PORT,
+        debug=True)
