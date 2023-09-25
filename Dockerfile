2b19201dd3e1d3cd6cca7256f8d02478fbda584a
Dani Quesada <daniela.dudyfit@gmail.com>
2023-09-25 23:25:47 +0200

    fixes

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,6 @@
 FROM python:3.8 AS bot
 COPY requirements.txt /app/
 WORKDIR /app
+COPY . /app
 RUN pip install -r requirements.txt
-COPY . .
-CMD ["python3", "bot.py", "--host", "0.0.0.0", "--port", "80"]
+CMD python3 bot.py  --hostname 0.0.0.0 --port $PORT
--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import os
 
 import telebot
 from dotenv import load_dotenv
-# from handlers import *
+from handlers import *
 
 def main():
     load_dotenv()
@@ -12,12 +12,11 @@ def main():
     if BOT_TOKEN:
         bot = telebot.TeleBot(BOT_TOKEN)
         
-        # initBot(bot,61695)
+        initBot(bot,61695)
         print("initiated bot")
         bot.infinity_polling()
     else:
         print('Error with BOT INIT')
-
 
 if __name__ == '__main__':
     main()
